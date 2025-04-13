f5a2365e2b99b8b56f15a9a46df2f0fd6c79fdb4
Windu <winduaji999@gmail.com>
2025-04-14 03:35:50 +0700

    Menggunakan serverless-http untuk Vercel deployment

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 // Vercel serverless entry point
+const serverless = require('serverless-http');
 const app = require('../vercel-server');
 
-// Export the Express app as a serverless function
-module.exports = app;
+// Export the Express app wrapped with serverless-http
+module.exports = serverless(app);
